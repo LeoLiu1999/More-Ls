26d58cfeaee246116841b6c36ca50515005e2348
bleung329 <bleung@stuy.edu>
2017-09-26 13:02:44 -0400

    Part 2

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask 
+from flask import Flask,render_template
+import util.occupation.py
 
 app = Flask(__name__)
+
 @app.route("/")
 def landing():
 	return 'Hi there, go to <a href="localhost:5000/occupations"> occupations </a>'
+
 @app.route("/occupations")
-	
+def occupation_temp():
+
 if app.__name__ == "__main__":
 	app.debug = True
 	app.run()
