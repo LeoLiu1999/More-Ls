be3447c9df0a354a93dfaaaff93533d924b0c5df
bleung329 <bleung@stuy.edu>
2017-09-26 14:40:58 -0400

    TemplateNotFound Error has been fixed.

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def landing():
 def occupation_temp():
 	csv = jobs.make_dict_from_csv('occupations.csv')
 	random_job = jobs.random_job(csv)
-	return render_template('occupation.html',job_dict=csv,random_job=random_job)
+	return render_template('occupations.html',job_dict=csv,random_job=random_job)
 
 if __name__ == "__main__":
 	app.debug = True
